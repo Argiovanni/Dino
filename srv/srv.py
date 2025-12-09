b7bbfcd66debdea8cd258d884f2787cef4b9aa3d
EmilieElo <emiel.bernard@outlook.fr>
2025-12-09 11:02:47 +0100

    Beginning web server

--- srv/srv.py
+++ srv/srv.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import can
 import json
 import time
 
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def select_user():
+    new_user_button = "New user"
+    return render_template('select-user.html',new_user_button=new_user_button)
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form['name']
+    
+    # Perform some processing with the form data
+    return 'Hello, ' + name + '!'
